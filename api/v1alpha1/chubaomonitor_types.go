95e08eeed866fc8195a4407cd54a84ccdd392872
root <2741403628@qq.com>
2020-08-01 20:00:39 +0800

    backup fot the first time

---
+++ api/v1alpha1/chubaomonitor_types.go
@@ -0,0 +1,73 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ChubaoMonitorSpec defines the desired state of ChubaoMonitor
+type ChubaoMonitorSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ChubaoMonitor. Edit ChubaoMonitor_types.go to remove/update
+	Sizep            int32                        `json:"sizep"`
+	Sizeg            int32                        `json:"sizeg"`
+	Imagep           string                       `json:"imagep"`
+	Imageg           string                       `json:"imageg"`
+	Portsp           []corev1.ServicePort         `json:"portsp,omitempty"`
+	Portsg           []corev1.ServicePort         `json:"portsg,omitempty"`
+	ImagePullPolicyp corev1.PullPolicy            `json:"imagePullPolicyp,omitempty"`
+	ImagePullPolicyg corev1.PullPolicy            `json:"imagePullPolicyg,omitempty"`
+	HostPath         *corev1.HostPathVolumeSource `json:"hostPath,omitempty"`
+}
+
+// ChubaoMonitorStatus defines the observed state of ChubaoMonitor
+type ChubaoMonitorStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// ChubaoMonitor is the Schema for the chubaomonitors API
+type ChubaoMonitor struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ChubaoMonitorSpec   `json:"spec,omitempty"`
+	Status ChubaoMonitorStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ChubaoMonitorList contains a list of ChubaoMonitor
+type ChubaoMonitorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ChubaoMonitor `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ChubaoMonitor{}, &ChubaoMonitorList{})
+}
---
+++ controllers/chubaomonitor_controller.go
@@ -0,0 +1,374 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	utilintstr "k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	cachev1alpha1 "ChubaoMonitorOperator/api/v1alpha1"
+)
+
+// ChubaoMonitorReconciler reconciles a ChubaoMonitor object
+type ChubaoMonitorReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=cache.example.com,resources=chubaomonitors,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cache.example.com,resources=chubaomonitors/status,verbs=get;update;patch
+
+func (r *ChubaoMonitorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("chubaomonitor", req.NamespacedName)
+
+	// your logic here
+
+	//my logic finished
+	return ctrl.Result{}, nil
+}
+
+func (r *ChubaoMonitorReconciler) deploymentforprometheus(m *cachev1alpha1.ChubaoMonitor) *appsv1.Deployment {
+	labels := labelsForChubaoMonitor(m.Name)
+	selector := &metav1.LabelSelector{MatchLabels: labels}
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(m, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "ChubaoMonitor",
+				}),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &m.Spec.Sizep,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: containerforprometheus(m),
+					Volumes:    volumeforprometheus(m),
+				},
+			},
+			Selector: selector,
+		},
+	}
+	// Set Prometheus instance as the owner and controller
+	ctrl.SetControllerReference(m, dep, r.Scheme)
+	return dep
+}
+
+func containerforprometheus(m *cachev1alpha1.ChubaoMonitor) []corev1.Container {
+	containerPorts := []corev1.ContainerPort{}
+	for _, svcPort := range m.Spec.Portsp {
+		cport := corev1.ContainerPort{}
+		cport.ContainerPort = svcPort.TargetPort.IntVal
+		containerPorts = append(containerPorts, cport)
+	}
+
+	return []corev1.Container{
+		{
+			Name:            "prometheus-pod",
+			Image:           m.Spec.Imagep,
+			Ports:           containerPorts,
+			ImagePullPolicy: m.Spec.ImagePullPolicyp,
+			Env: []corev1.EnvVar{
+				{Name: "TZ", Value: "Asia/Shanghai"},
+			},
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "monitor-config",
+					MountPath: "/etc/prometheus/prometheus.yml",
+					SubPath:   "prometheus.yml",
+				},
+				{
+					Name:      "prometheus-data",
+					MountPath: "/prometheus-data",
+				},
+			},
+		},
+	}
+}
+
+func serviceforprometheus(m *cachev1alpha1.ChubaoMonitor) *corev1.Service {
+	labels := labelsForChubaoMonitor(m.Name)
+
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "prometheus-service",
+			Namespace: m.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(m, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "ChubaoMonitor",
+				}),
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port:       m.Spec.Portsp[0].Port,
+					TargetPort: m.Spec.Portsp[0].TargetPort,
+					Protocol:   "TCP",
+				},
+			},
+			Selector: labels,
+		},
+	}
+
+}
+
+func volumeforprometheus(m *cachev1alpha1.ChubaoMonitor) []corev1.Volume {
+	var defaultmode int32 = 0555
+	return []corev1.Volume{
+		{
+			Name: "monitor-config",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "monitor-config",
+					},
+					DefaultMode: &defaultmode,
+				},
+			},
+		},
+		{
+			Name: "prometheus-data",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: m.Spec.HostPath,
+			},
+		},
+	}
+}
+
+func (r *ChubaoMonitorReconciler) deploymentforgrafana(m *cachev1alpha1.ChubaoMonitor) *appsv1.Deployment {
+	labels := labelsForChubaoMonitor(m.Name)
+	selector := &metav1.LabelSelector{MatchLabels: labels}
+	var defaultmode int32 = 0555
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(m, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "ChubaoMonitor",
+				}),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &m.Spec.Sizeg,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: containerforgrafana(m),
+					Volumes: []corev1.Volume{
+						{
+							Name: "monitor-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "monitor-config",
+									},
+									DefaultMode: &defaultmode,
+								},
+							},
+						},
+						{Name: "grafana-persistent-storage",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+					},
+				},
+			},
+			Selector: selector,
+		},
+	}
+	// Set ChubaoMonitor instance as the owner and controller
+	ctrl.SetControllerReference(m, dep, r.Scheme)
+	return dep
+}
+
+func containerforgrafana(m *cachev1alpha1.ChubaoMonitor) []corev1.Container {
+	containerPorts := []corev1.ContainerPort{}
+	for _, svcPort := range m.Spec.Portsg {
+		cport := corev1.ContainerPort{}
+		cport.ContainerPort = svcPort.TargetPort.IntVal
+		containerPorts = append(containerPorts, cport)
+	}
+	var privileged bool = true
+	return []corev1.Container{
+		{
+			Name:            "grafana-pod",
+			Image:           m.Spec.Imageg,
+			Ports:           containerPorts,
+			ImagePullPolicy: m.Spec.ImagePullPolicyg,
+			SecurityContext: &corev1.SecurityContext{
+				Privileged: &privileged,
+			},
+			Env: []corev1.EnvVar{
+				{
+					Name:  "GF_AUTH_BASIC_ENABLED",
+					Value: "true",
+				},
+				{
+					Name:  "GF_AUTH_ANONYMOUS_ENABLED",
+					Value: "false",
+				},
+				{
+					Name:  "GF_SECURITY_ADMIN_PASSWORD",
+					Value: "123456",
+				},
+			},
+			ReadinessProbe: &corev1.Probe{
+				Handler: corev1.Handler{
+					HTTPGet: &corev1.HTTPGetAction{
+						Path: "/login",
+						Port: utilintstr.IntOrString{
+							IntVal: 3000,
+						},
+					},
+				},
+			},
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "grafana-persistent-storage",
+					MountPath: "/var/lib/grafana",
+				},
+				{
+					Name:      "monitor-config",
+					MountPath: "/grafana/init.sh",
+					SubPath:   "init.sh",
+				},
+				{
+					Name:      "monitor-config",
+					MountPath: "/etc/grafana/grafana.ini",
+					SubPath:   "grafana.ini",
+				},
+				{
+					Name:      "monitor-config",
+					MountPath: "/etc/grafana/provisioning/dashboards/chubaofs.json",
+					SubPath:   "chubaofs.json",
+				},
+				{
+					Name:      "monitor-config",
+					MountPath: "/etc/grafana/provisioning/dashboards/dashboard.yml",
+					SubPath:   "dashboard.yml",
+				},
+				{
+					Name:      "monitor-config",
+					MountPath: "/etc/grafana/provisioning/datasources/datasource.yml",
+					SubPath:   "datasource.yml",
+				},
+			},
+		},
+	}
+}
+
+func serviceforgrafana(m *cachev1alpha1.ChubaoMonitor) *corev1.Service {
+
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "grafana-service",
+			Namespace: m.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(m, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "ChubaoMonitor",
+				}),
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port:       m.Spec.Portsg[0].Port,
+					TargetPort: m.Spec.Portsg[0].TargetPort,
+					Protocol:   "TCP",
+				},
+			},
+			Selector: map[string]string{
+				"app": "grafana",
+			},
+		},
+	}
+
+}
+
+// labelsForChubaoMonitorOperator returns the labels for selecting the resources
+// belonging to the given chubaomonitor CR name.
+func labelsForChubaoMonitor(name string) map[string]string {
+	return map[string]string{"app": name}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
+}
+
+func (r *ChubaoMonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&cachev1alpha1.ChubaoMonitor{}).
+		Owns(&appsv1.Deployment{}).
+		Complete(r)
+}
